3febf225d84a4318bb01d9584c33bcef084a0fe2
aloRanking <adigunlateef02@gmail.com>
2021-05-03 05:40:23 +0100

    auth controller for add token to mutation or query string

---
+++ lib/controllers/authentication_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class AuthenticationController extends GetxController {
+  var token = ''.obs;
+
+//String tt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MDhhYzZiZGFlMzUyMjAwMTU3ZDEwMTEiLCJpYXQiOjE2MTk4Nzg3MDksImV4cCI6MTYxOTk2NTEwOX0.KYy0FB6k65EYCyIp4QHGpumJyWHw06pDYVeuKftCt9s';
+
+}
